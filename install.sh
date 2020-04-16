ee838d6f8f9c245d714ba74520609adcafd9b0b6
Suhan Park <psh117@snu.ac.kr>
2020-04-16 18:50:32 +0900

    update

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone --recursive https://github.com/psh117/assembly_dual_controllers
+git clone --recursive https://github.com/psh117/assembly_dummy_controller
+git clone --recursive https://github.com/psh117/assembly_env_description
+git clone --recursive https://github.com/psh117/assembly_knowledge
+git clone --recursive https://github.com/psh117/assembly_moveit_config
+git clone --recursive https://github.com/psh117/assembly_msgs
+git clone --recursive https://github.com/psh117/assembly_task_manager
