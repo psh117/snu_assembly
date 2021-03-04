3fd6ec0b04114fea2994d4c1b37016f774ee3b29
Suhan Park <psh117@snu.ac.kr>
2021-03-04 10:10:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-git clone --recursive https://github.com/psh117/assembly_dual_controllers
+mkdir -p ~/catkin_ws/src/snu_assembly
+cd ~/catkin_ws/src
+
+git clone --recursive https://github.com/psh117/constrained_motion_planning -b v1.2.0
+git clone --recursive https://github.com/psh117/assembly_dual_controllers -b v1.2.0
 git clone --recursive https://github.com/psh117/assembly_dummy_controller
-git clone --recursive https://github.com/psh117/assembly_env_description
-git clone --recursive https://github.com/psh117/assembly_knowledge
-git clone --recursive https://github.com/psh117/assembly_moveit_config
-git clone --recursive https://github.com/psh117/assembly_msgs
-git clone --recursive https://github.com/psh117/assembly_task_manager
+git clone --recursive https://github.com/psh117/assembly_env_description -b v1.2.0
+git clone --recursive https://github.com/psh117/assembly_knowledge -b v1.2.0
+git clone --recursive https://github.com/psh117/assembly_moveit_config -b v1.2.0
+git clone --recursive https://github.com/psh117/assembly_msgs -b v1.2.0
+git clone --recursive https://github.com/psh117/assembly_task_manager -b v1.2.0
+
+cd ~/catkin_ws/
+
+catkin build
