571dc3533a386ae9371234ea9ab749f759e6faff
Suhan Park <psh117@snu.ac.kr>
2021-03-04 13:06:40 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/catkin_ws/src/snu_assembly
-cd ~/catkin_ws/src
+cd ~/catkin_ws/src/snu_assembly
 
 git clone --recursive https://github.com/psh117/constrained_motion_planning -b v1.2.0
 git clone --recursive https://github.com/psh117/assembly_dual_controllers -b v1.2.0
